64778558a670d8ee82179322f0ce2f3dcafe797b
yuliuyuan <yuliuyuan@bytedance.com>
2021-04-13 19:02:14 +0800

    feat: update page
    Change-Id: I7096bd38cd8bdcacd33afa667cc5c232600b67b2

--- src/pages/manage/views/NavMenuAdmin.vue
+++ src/pages/manage/views/NavMenuAdmin.vue
@@ -1,5 +1,13 @@
 <template>
   <el-menu :default-openeds="['1', '3']" class="el-menu-demo" mode="horizontal"  active-text-color="DodgerBlue" router="true" >
+    <el-submenu index="/admin/news">
+      <template #title><i class="el-icon-s-grid"></i>新闻管理</template>
+      <el-menu-item-group>
+        <el-menu-item index="/admin/news/list">新闻列表管理</el-menu-item>
+        <el-menu-item index="/admin/news/add ">添加新闻</el-menu-item>
+      </el-menu-item-group>
+    </el-submenu>
+
     <el-submenu index="/admin/user">
       <template #title><i class="el-icon-s-grid"></i>用户管理</template>
       <el-menu-item-group>
@@ -13,6 +21,22 @@
       <el-menu-item-group>
         <el-menu-item index="/admin/problem/list">题目列表管理</el-menu-item>
         <el-menu-item index="/admin/problem/add">添加题目</el-menu-item>
+      </el-menu-item-group>
+    </el-submenu>
+
+    <el-submenu index="/admin/contest">
+      <template #title><i class="el-icon-s-grid"></i>竞赛管理</template>
+      <el-menu-item-group>
+        <el-menu-item index="/admin/contest/list">竞赛列表管理</el-menu-item>
+        <el-menu-item index="/admin/contest/add ">添加竞赛</el-menu-item>
+      </el-menu-item-group>
+    </el-submenu>
+
+    <el-submenu index="/admin/contest">
+      <template #title><i class="el-icon-s-grid"></i>实验管理</template>
+      <el-menu-item-group>
+        <el-menu-item index="/admin/contest/list">实验列表管理</el-menu-item>
+        <el-menu-item index="/admin/contest/add ">添加实验</el-menu-item>
       </el-menu-item-group>
     </el-submenu>
 

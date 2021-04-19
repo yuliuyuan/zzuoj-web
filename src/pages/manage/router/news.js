5d15e8eab5245782f255c68f89a32b1e5707a6a4
yuliuyuan <yuliuyuan@bytedance.com>
2021-04-19 21:42:00 +0800

    feat: update news backed
    Change-Id: I8c4da3752a1bb47a6cf1f355ef4e92c01a30b430

--- src/pages/manage/router/news.js
+++ src/pages/manage/router/news.js
@@ -1,5 +1,6 @@
 import newsList from '@manage/views/news/NewsList.vue'
 import news from '@manage/views/news/News.vue'
+import newsAdd from '@manage/views/news/NewsAdd.vue'
 import Global from '@/Global.js'
 
 export default [
@@ -17,5 +18,12 @@ export default [
             manage: news
         },
         props: true
+    },
+    {
+        path: Global.admin_base_url_prefix + '/news/add',
+        name: 'addNews',
+        components: {
+            manage: newsAdd
+        },
     }
 ]
---
+++ src/pages/manage/views/news/NewsAdd.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="Main">
+    <div class="Title">
+      <p>添加新闻</p>
+    </div>
+    <el-divider></el-divider>
+    <div class="Content">
+      <el-form ref="form" :model="form"  label-width="80px" >
+        <el-form-item class="ContentTitle">
+          <el-input v-model="form.title" style="width: 300px;" placeholder="请输入文章题目"></el-input>
+        </el-form-item>
+
+        <el-form-item class="ContentMain">
+          <el-input v-model="form.content"  placeholder="请输入文章内容" type="textarea" :rows="25"></el-input>
+        </el-form-item>
+
+      </el-form>
+      <div class="Button">
+        <el-button type="primary" @click="handleAddNews(this.form)">提交</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+import api from "@/util/api.js";
+
+export default {
+  name: 'ProblemAdd',
+  data() {
+    return {
+      form: {
+        title: '',
+        content: '',
+      },
+    }
+  },
+
+  methods: {
+    handleAddNews(data){
+      api.addProblem(data).then( res => {
+        console.log(res);
+      }).catch(err => {
+        console.log("add problem err" + err);
+        //todo: 做个兜底
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+.ContentTitle {
+  text-align: center;
+}
+.ContentMain {
+}
+
+.Button {
+  text-align: right;
+}
+
+</style>
